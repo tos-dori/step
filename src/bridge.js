b7e4dd00cb24695950db08f026f3b65a6896dc88
tos-dori <pks020428@gmail.com>
2026-07-28 22:14:18 +0900

    Expose Step data safety bridge

--- src/bridge.js
+++ src/bridge.js
@@ -4,6 +4,11 @@ window.StepSyncApp={
       hasLocalCloudData:function(){var data=stateForCloud();return !!(data.activeId||(Array.isArray(data.tasks)&&data.tasks.length))},
       applyCloudState:function(remote){applyCloudState(remote);renderAll()},
       isLocalInputActive:isLocalInputActive,
+      isLocalStateSafe:function(){return !window.StepDataSafety||window.StepDataSafety.isSafe()},
+      localSafetyIssue:function(){return window.StepDataSafety?window.StepDataSafety.issue():""},
+      snapshotLocal:function(reason){return window.StepDataSafety?window.StepDataSafety.snapshotCurrent(reason):false},
+      consumeOperation:function(){return window.StepDataSafety?window.StepDataSafety.consumeOperation():""},
+      destructiveChange:function(previous,next){return window.StepDataSafety?window.StepDataSafety.destructiveChange(previous,next):false},
       renderAll:renderAll,
       toast:toast
     };
